c3a5b2264518ce491c78e17498dd1893e7c4ff54
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-15 15:18:17 -0400

    created itemDeleteSubmit functionality

--- app.js
+++ app.js
@@ -75,6 +75,18 @@ const ItemCtrl = (function() {
             });
             return found;
         },
+        deleteItem: function(id){
+            // Get ids
+            const ids = data.items.map(function(item) {
+                return item.id;
+            });
+
+            // Get Index
+            const index = ids.indexOf(id);
+
+            // Remove item
+            data.items.splice(index, 1);
+        },
         setCurrentItem: function(item) {
             data.currentItem = item;
         },
@@ -180,6 +192,13 @@ const UICtrl = (function() {
                     `;
                 }
             });
+        },
+        deleteListItem: function(id) {
+            const itemID = `#item-${id}`;
+            const item = document.querySelector(itemID);
+            item.remove();
+
+
         },
         clearInput: function() {
             document.querySelector(UISelectors.itemNameInput).value = '';
@@ -312,6 +331,27 @@ const App = (function(ItemCtrl, UICtrl){
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
 
+        UICtrl.clearEditState();
+
+        e.preventDefault();
+    }
+
+    // Delete Button event
+    const itemDeleteSubmit = function(e) {
+        // Get currentItem
+        const currentItem = ItemCtrl.getCurrentItem();
+
+        // Delete from data structure
+        ItemCtrl.deleteItem(currentItem.id);
+
+        // Delete from UI
+        UICtrl.deleteListItem(currentItem.id);
+
+        // Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // Add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+        
         UICtrl.clearEditState();
 
         e.preventDefault();
