e9716268020e572a214c09f8cd3030c1a85044d6
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-04-14 20:11:21 -0400

    created setCurrentItem method

--- app.js
+++ app.js
@@ -60,6 +60,9 @@ const ItemCtrl = (function() {
             });
             return found;
         },
+        setCurrentItem: function(item) {
+            data.currentItem = item;
+        },
         getTotalCalories: function() {
             let total = 0;
 
@@ -216,6 +219,9 @@ const App = (function(ItemCtrl, UICtrl){
 
             // Get item
             const itemToEdit = ItemCtrl.getItemById(id);
+
+            // Set current item
+            ItemCtrl.setCurrentItem(itemToEdit);
         }
 
         e.preventDefault();
